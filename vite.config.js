c59044db6e08ed7535c50e017b615a0da8f7d850
Shady776 <temitayosalisu42@gmail.com>
2026-06-16 13:28:31 +0100

    fix: allow render host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 10000
+    port: 10000,
+    allowedHosts: ['nacosbughuntgame.onrender.com']
   }
 })
